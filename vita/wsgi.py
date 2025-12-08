db63697f8b9bca2816ae56edad0ee614ec7dc777
Frank Matranga <thefrankmatranga@gmail.com>
2025-12-07 20:31:45 -0500

    feat: Format and core app

--- vita/wsgi.py
+++ vita/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vita.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vita.settings")
 
 application = get_wsgi_application()
